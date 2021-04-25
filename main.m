c5d8af593363e33a90f5c86089af9784576ea89b
Christian Nelson <christian.nelson@eit.lth.se>
2021-04-25 09:13:03 +0200

    Put sections in main.m.

--- main.m
+++ main.m
@@ -1,5 +1,7 @@
+%% Clear workspace and close all open figures.
 clear; close all; clc
 
+%% Setup the environment, scatterers, walls etc.
 % 11 new variables in Workspace
 % A, chi1, chi2, chi3, H, p1, p2, streetWidth, W, n_leftWall, n_rightWall
 SetupEnvironment
@@ -18,17 +20,16 @@ RxPos       = [0, 5, 0]';
 TxStartPos  = [0, H-5, 0]';
 TxVel       = [0, -10, 0]'; 
 
-
+%% Plot the simulation start
 fh = figure(); hold on;
 rectangle('Position', [p1(1), p1(2), W, H])
 rectangle('Position', [p2(1), p2(2), W, H])
 PlotScatterers(fh, leftWall_1,  leftWall_2,  leftWall_3, ...
                    rightWall_1, rightWall_2, rightWall_3)
-
 scatter(RxPos(1), RxPos(2), 'filled', 'MarkerFaceColor',[1 0 0])
 scatter(TxStartPos(1), TxStartPos(2), 'filled', 'MarkerFaceColor',[0 1 0])
 
-
+%% Do some mirroring and then the projections on to Rx.
 length_1st = length(leftWall_1);
 length_2nd = length(leftWall_2);
 length_3rd = length(leftWall_3);
