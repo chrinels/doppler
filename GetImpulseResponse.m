3dd16f9d7dc54ab762ce8b8e9ed01df30cd810c9
Christian Nelson <christian.nelson@eit.lth.se>
2021-04-29 00:31:15 +0200

    Work In Progress!

--- GetImpulseResponse.m
+++ GetImpulseResponse.m
@@ -3,6 +3,21 @@ function [ImpulseResponse] = GetImpulseResponse(EnvironmentParams, SimulationPar
 %   Detailed explanation goes here
     [Delay, Doppler] = SimulateDelayAndDoppler(SimulationParams, MPCs, Tx, Rx);
     
+    f = Tx.Frequency;
+    lambda = Tx.Frequency/physconst('LightSpeed');
+    
+    delay = cell2mat({Delay.FirstOrder}'); % [m]
+    delay(delay==0)=NaN;
+    tau = delay/physconst('LightSpeed');
+    
+    
+    doppler = cell2mat({Doppler.FirstOrder}');
+    doppler(delay==0) = NaN;
+    
+    exp(-2j*pi*doppler);
+    first = lambda./(4*pi*delay).*EnvironmentParams.G0_1(size(delay)).*exp(2j*pi*f*tau);
+    
+    ImpulseResponse = 0;
     
 end
 
